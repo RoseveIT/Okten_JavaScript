1608662738782635bc0eb2022d72148107571010
RoseveIT <62Bip23_Sorta557_qq>
2022-02-01 19:29:32 +0200

    1

---
+++ homework/losson1/main.js
@@ -0,0 +1,145 @@
+//Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//Вивести кожну змінну за допомогою: console.log , alert, document.write
+//
+// let hello = "hello";
+// alert(hello);
+// document.write(`<div>${hello}</div>`);
+//
+// let owu = "owu";
+// alert(owu);
+// document.write(`<div>${owu}</div>`);
+//
+// let com = "com";
+// alert(com);
+// document.write(`<div>${com}</div>`);
+//
+// let ua = "ua";
+// alert(ua);
+// document.write(`<div>${ua}</div>`);
+//
+// let one = 1;
+// alert(one);
+// document.write(`<div>${one}</div>`);
+//
+// let z = 10;
+// alert(z);
+// document.write(`<div>${z}</div>`);
+//
+// let q = -999;
+// alert(q);
+// document.write(`<div>${q}</div>`);
+//
+// let a = 123;
+// alert(a);
+// document.write(`<div>${a}</div>`);
+//
+// let pi = 3.14;
+// alert(pi);
+// document.write(`<div>${pi}</div>`);
+//
+// let e = 2.7;
+// alert(e);
+// document.write(`<div>${e}</div>`);
+//
+// let w = 16;
+// alert(w);
+// document.write(`<div>${w}</div>`);
+//
+// let t = true;
+// alert(t);
+// document.write(`<div>${t}</div>`);
+//
+// let f = false;
+// alert(f);
+// document.write(`<div>${f}</div>`);
+// console.log(hello, owu, com, ua, one, z, q, a, pi, e, w, t, f);
+
+
+
+//Переприсвоїти кожній змінній з завдання значення на довільне.
+//Вивести кожну змінну за допомогою: console.log , alert, document.write
+// hello = "bye";
+// owu = "uwo";
+// com = "moc";
+// ua = "en";
+// one = 1.0;
+// z = 100;
+// q = 999;
+// a = 321;
+// pi = 3;
+// e = 44;
+// w = 4;
+// t = false;
+// f = true;
+// document.write(`
+// <div>${hello}</div>
+// <div>${owu}</div>
+// <div>${com}</div>
+// <div>${ua}</div>
+// <div>${one}</div>
+// <div>${z}</div>
+// <div>${q}</div>
+// <div>${a}</div>
+// <div>${pi}</div>
+// <div>${e}</div>
+// <div>${w}</div>
+// <div>${t}</div>
+// <div>${f}</div>`);
+// console.log(hello, owu, com, ua, one, z, q, a, pi, e, w, t, f);
+
+
+
+//Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person. (Не об'єкт)
+// let person = {
+//     firstName: 'Rostyslav',
+//     middleName: 'Maryanovich',
+//     secondName: 'Luno'
+// }
+// console.log(person);
+// //document.write(person.firstName + ' ' +person.middleName+ ' ' + person.secondName );
+
+
+
+//За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
+//Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
+// let firstName = prompt("Ім'я");
+// let middleName = prompt("По батькові");
+// let lastName = prompt("Прізвище");
+// let age = prompt("Вік");
+//
+// document.write('Вітаю ' + ' ' + lastName + ' ' + firstName + ' ' + middleName + ' ' + 'тобі' +' '+ age +' '+ 'років');
+
+
+
+//За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+// let a = 100;
+// let b = '100';
+// let c = true;
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+
+
+//Визначити відповідний оператор в виразах що б вийшов відповідний результат.
+//В однакових виразах не використовувати однакові оператори!!!
+// console.log('5 != 6', '-',  5 != 6);  //true
+// console.log('5 == 6', '-',  5 == 6);  //false
+// console.log('5 >= 6', '-',  5 >= 6);  //false
+// console.log('5 > 6', '-',  5 > 6);  //false
+// console.log('10 == 10', ' ', 10 == 10);   //true
+// console.log('10 >= 10', ' ', 10 >= 10);   //true
+// console.log('10 <= 10', ' ', 10 <= 10);   //true
+// console.log('10 != 10', ' ', 10 != 10);   //false
+// console.log('10 > 10', ' ', 10 > 10);   //false
+// console.log('10 < 10', ' ', 10 < 10);   //false
+// console.log('123 ? \'123\'', ' ', 123 != '123');   //false
+// console.log('123 ? \'123\'', ' ', 123 == '123');   //true
+
+//Подумайте Какие значения выведет в окно браузера следующий фрагмент кода?  и почему?
+//     let str = "2";
+//     let a = 5;
+//     document.write(str + a + "<br/>");
+//     document.write(str - a + "<br/>");
+//     document.write(str * "2" + "<br/>");
+//     document.write(str / 2 + "<br/>");
+
