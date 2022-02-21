aa21974d99fff5235846614d118aa74b5d50b982
RoseveIT <62Bip23_Sorta557_qq>
2022-02-21 13:31:49 +0200

    8

---
+++ homework/lesson8/main.js
@@ -0,0 +1,40 @@
+//- Напишіть код,  котрий :
+//     -- отримує текст з параграфа з id "content"
+// let contentId = document.getElementById('content');
+// console.log(contentId)
+
+//-- отримує текст з блоку з id "rules"
+// let relText = document.getElementById("rules")
+// console.log(relText)
+
+//-- замініть текст параграфа з id 'content' на будь-який інший
+// let changeTextContent = document.getElementById('content');
+// changeTextContent.innerText = 'hello'
+
+// -- замініть текст параграфа з id 'rules' на будь-який інший
+// let changeTextId = document.getElementById('rules');
+// changeTextId.innerText = 'world'
+
+//-- змініть кожному елементу колір фону на червоний
+// let everyElementChangeColor = document.body.children;
+// for (let child of everyElementChangeColor){
+//     child.style.background = 'black'
+// }
+
+//-- змініть кожному елементу колір тексту на синій
+// let everyElementChCrText = document.body.children;
+// for (let child of everyElementChCrText){
+//     child.style.color = 'silver'
+// }
+
+// -- отримати весь список класів елемента з id=rules і вивести їх в console.log
+// let classListRulesAll = document.getElementsByClassName('fc_rules')
+// for (let classChild of classListRulesAll){
+//     console.log(classChild)
+// }
+
+//-- поміняти колір тексту у всіх елементів fc_rules на червоний
+// let everyRuleChangeColorText = document.getElementsByClassName("fc_rules")
+// for (let ruleChild of everyRuleChangeColorText){
+//     ruleChild.style.color = 'red'
+// }
