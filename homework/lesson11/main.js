fce4c40a3c9020481cc8c1849ff4f2fbc944c999
RoseveIT <62Bip23_Sorta557_qq>
2022-03-09 19:16:45 +0200

    11

---
+++ homework/lesson11/main.js
@@ -0,0 +1,31 @@
+//-створити форму з інпутами для name та age.
+// При відправці форми записати об'єкт в localstorage
+// let form = document.forms[0]
+// form.onsubmit = function (e){
+//     e.preventDefault();
+// }
+let name = document.forms.userF.name;
+let age = document.forms.userF.age;
+let btn = document.getElementById('button')
+
+
+let submit = function (userName, userAge) {
+    let user = {
+        name: userName,
+        age: userAge
+    }
+    localStorage.setItem('key', JSON.stringify(user))
+}
+btn.onclick = function (){
+    submit(name.value, age.value)
+}
+
+
+
+//-створити форму з інпутами для model,type та volume автівки.
+// при відпарвці форми об'єкти зберігаються в масиві в локальному сховищі.
+// let model = document.forms.carForm.model;
+// let type = document.forms.carForm.type;
+// let volume = document.forms.carForm.volume;
+// let btn = document.getElementById('btn')
+
