cb36da3ba191db85d7201cda84a37c0bee3ade72
RoseveIT <62Bip23_Sorta557_qq>
2022-03-09 19:52:50 +0200

    11

--- homework/lesson11/main.js
+++ homework/lesson11/main.js
@@ -4,28 +4,37 @@
 // form.onsubmit = function (e){
 //     e.preventDefault();
 // }
-let name = document.forms.userF.name;
-let age = document.forms.userF.age;
-let btn = document.getElementById('button')
-
-
-let submit = function (userName, userAge) {
-    let user = {
-        name: userName,
-        age: userAge
-    }
-    localStorage.setItem('key', JSON.stringify(user))
-}
-btn.onclick = function (){
-    submit(name.value, age.value)
-}
+// let name = document.forms.userF.name;
+// let age = document.forms.userF.age;
+// let btn = document.getElementById('button')
+//
+//
+// let submit = function (userName, userAge) {
+//     let user = {
+//         name: userName,
+//         age: userAge
+//     }
+//     localStorage.setItem('key', JSON.stringify(user))
+// }
+// btn.onclick = function (){
+//     submit(name.value, age.value)
+// }
 
 
 
 //-створити форму з інпутами для model,type та volume автівки.
 // при відпарвці форми об'єкти зберігаються в масиві в локальному сховищі.
-// let model = document.forms.carForm.model;
-// let type = document.forms.carForm.type;
-// let volume = document.forms.carForm.volume;
-// let btn = document.getElementById('btn')
+let model = document.forms.carForm.model;
+let type = document.forms.carForm.type;
+let volume = document.forms.carForm.volume;
+let btn = document.getElementById('btn')
 
+let submit = function (modelCar, typeCar, volumeCar){
+    let auto = localStorage.getItem('car');
+    let array = JSON.parse(auto) || []
+    array.push({modelCar,typeCar,volumeCar})
+    localStorage.setItem('car', JSON.stringify(array))
+}
+btn.onclick = function (){
+    submit(model.value, type.value, volume.value);
+}
